48fede9eb2231dcb0dfa312e115ae95716cfa220
Suborno22 <dsuborno0@gmail.com>
2023-11-09 17:40:04 +0530

    deleted admins

--- Server/script.sql
+++ Server/script.sql
@@ -1,8 +1,8 @@
 /* drop database if exists Library_manager; */
 
 use Library_manager;
+show tables;
 
-select * from Library_manager.Users;
 
 /* delete from Library_manager.Users where id = 1; */
 
