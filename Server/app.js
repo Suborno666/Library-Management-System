af31612f8c4501f26a70d42f8b239d30811f4f40
Suborno22 <dsuborno0@gmail.com>
2023-11-22 14:02:23 +0530

    hid the username and password

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express()
 const userRoutes = require("./routes/userRoutes")
 const adminRoutes = require("./routes/adminRoutes")
 const bookRoutes = require("./routes/bookRoutes")
 const authorRoutes = require("./routes/authorRoutes")
+const path = require('path')
+const filepath = path.join(__dirname,"/../Client")
 
 app.use(express.json())
+app.set('view engine','hbs')
 
 app.use('/users',userRoutes)
 app.use('/book',bookRoutes)
