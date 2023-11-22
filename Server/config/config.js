58da9444e5e08a28e6cc2869b55f2c93038e62cf
Suborno22 <dsuborno0@gmail.com>
2023-11-22 18:21:27 +0530

    added config.js

---
+++ Server/config/config.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+  dialect: process.env.DB_DIALECT || "mysql",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: process.env.DB_PORT || 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+};
+
+console.log(config); // Add this line for debugging
+
+module.exports = {
+  url: process.env.DB_CONNECTION_URL,
+  development: { ...config, database: 'Library_manager' },
+  test: { ...config, database: process.env.DB_TEST_NAME },
+  production: { ...config, database: process.env.DB_PROD_NAME },
+};
