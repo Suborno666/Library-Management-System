a286e96a28a5fe436b57ed01860628a888f368a8
Suborno22 <dsuborno0@gmail.com>
2023-11-23 12:51:03 +0530

    updated for deployment

--- Server/app.js
+++ Server/app.js
@@ -5,11 +5,8 @@ const userRoutes = require("./routes/userRoutes")
 const adminRoutes = require("./routes/adminRoutes")
 const bookRoutes = require("./routes/bookRoutes")
 const authorRoutes = require("./routes/authorRoutes")
-const path = require('path')
-const filepath = path.join(__dirname,"/../Client")
 
 app.use(express.json())
-app.set('view engine','hbs')
 
 app.use('/users',userRoutes)
 app.use('/book',bookRoutes)
