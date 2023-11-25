a510cb3cebe5a74ab5e8b1771e348ddee9f24a0e
Suborno22 <dsuborno0@gmail.com>
2023-11-25 09:10:19 +0530

    modified config.js

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,19 +1,41 @@
 const dotenv = require('dotenv');
-dotenv.config();
+const path = require('path');
 
-const config = {
-  dialect: process.env.DB_DIALECT || "mysql",
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD,
-};
-
-// console.log(config);
+// Load environment variables from .env file in the directory above
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 module.exports = {
-  url: process.env.DB_CONNECTION_URL,
-  development: { ...config, database: process.env.DB_DEVELOPMENT_NAME },
-  test: { ...config, database: process.env.DB_TEST_NAME },
-  production: { ...config, database: process.env.DB_PROD_NAME },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DEVELOPMENT_NAME,
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: process.env.CI_DB_HOST || '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  }
 };
