435fcb7c7fbb150ff30efb2cfe19495ef2efc6d5
Suborno22 <dsuborno0@gmail.com>
2023-11-25 12:06:52 +0530

    Updated config.js

--- Server/config/config.js
+++ Server/config/config.js
@@ -9,11 +9,10 @@ const config = {
   password: process.env.DB_PASSWORD,
 };
 
-console.log(config);
-
 module.exports = {
   url: process.env.DB_CONNECTION_URL,
   development: { ...config, database: process.env.DB_DEVELOPMENT_NAME },
   test: { ...config, database: process.env.DB_TEST_NAME },
   production: { ...config, database: process.env.DB_PROD_NAME },
 };
+console.log(config);
